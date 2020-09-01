9f9e5c8f8383ae01537ace2681e958ff24832356
maciej-witkowski <=maciej.jan.witkowski@gmail.com>
2020-09-02 01:01:16 +0200

    prymitywne metody /dodaj/ i /usuń/ na stosie

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Card from "./Card";
+import Stack from "./Stack";
 
 const kolory = ["♠", "♥", "♦", "♣"];
 const figury = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
 function App() {
   return (
-      <div>
-        <Card kolor={kolory[1]} figura={figury[2]}/>
-        <Card kolor={kolory[3]} figura={figury[9]}/>
+      <div className="app">
+        {/*<Card kolor={kolory[1]} figura={figury[2]}/>*/}
+        {/*<Card kolor={kolory[3]} figura={figury[9]}/>*/}
+        <Stack/>
       </div>
   );
 }
--- src/Stack.js
+++ src/Stack.js
@@ -1,14 +1,29 @@
 import React, {useState} from "react";
 import Card from "./Card";
 
+const kolory = ["♠", "♥", "♦", "♣"];
+const figury = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
 function Stack() {
 
     const [state, setState] = useState([]);
 
-    let test = <Card />
+    const addCard = (card) => {
+        setState([...state, card]);
+    }
+
+    const removeCard = () => {
+        let copy = [...state];
+        copy.pop();
+        setState(copy);
+    }
 
     return (
-        {test}
+        <div>
+            <button onClick={() => addCard(<Card kolor={kolory[1]} figura={figury[2]}/>)}>Dodaj</button>
+            <button onClick={removeCard}>Usuń</button>
+            {state}
+        </div>
     )
 }
 
